cffaf953bece4ae2f3f1fbe4c59c91bf7ef4f67e
AboAlwafaa <48411694+AboAlwafaa@users.noreply.github.com>
2019-03-10 16:06:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = '#'
+var adminprefix = 'Y'
 
 
-const developers = ["ID","ID"]
+const developers = ["440141438236360705"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
